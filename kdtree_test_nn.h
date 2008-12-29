208ee9733243e3e503cabcf0bff3812860b13e9b
unknown <mwashenb@gmail.com>
2008-12-28 20:17:44 -0500

    more beginnings of nearest neighbor tests

--- kdtree_test_nn.h
+++ kdtree_test_nn.h
@@ -18,6 +18,16 @@ public:
 	{
 		kdt = kdtree_new(0);
 		kdtree_add(kdt, 5.0, 5.0, "center");
+		kdtree_add(kdt, 0.0, 0.0, "bottomleft");
+		kdtree_add(kdt, 0.0, 5.0, "left");
+		kdtree_add(kdt, 0.0, 10.0, "topleft");
+		kdtree_add(kdt, 5.0, 10.0, "top");
+		kdtree_add(kdt, 10.0, 10.0, "topright");
+		kdtree_add(kdt, 10.0, 5.0, "right");
+		kdtree_add(kdt, 10.0, 0.0, "bottomright");
+		kdtree_add(kdt, 5.0, 0.0, "bottom");
+	}
+	void test_exact_center() {
 	}
 	void tearDown()
 	{
