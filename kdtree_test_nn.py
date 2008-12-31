3d4c57940faf91c736898e43dcd643603be131f2
unknown <mwashenb@gmail.com>
2008-12-30 23:19:39 -0500

    small test to demo the limited functionality present in python nn impl

--- kdtree_test_nn.py
+++ kdtree_test_nn.py
@@ -4,10 +4,19 @@ import kdtree
 class TestKDTreeNN(unittest.TestCase):
 
 	def setUp(self):
-		pass
+		self.k = kdtree.KDTree()
+		self.k[5.0, 5.0] = 'center'
+		self.k[0.0, 0.0] = 'bottomleft'
+		self.k[0.0, 5.0] = 'left'
+		self.k[0.0, 10.0] = 'topleft'
+		self.k[5.0, 10.0] = 'top'
+		self.k[10.0, 10.0] = 'topright'
+		self.k[10.0, 5.0] = 'right'
+		self.k[10.0, 0.0] = 'bottomright'
+		self.k[5.0, 0.0] = 'bottom'
 
-	def test(self):
-		pass
+	def test_exact_center(self):
+		n = self.k.nn(5.0, 5.0)
 
 if __name__ == '__main__':
 	unittest.main()
